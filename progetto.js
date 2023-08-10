0ed9b8729467d020a22a2683022ad490dfb7d1d0
Daniele <daniele.2002.ddl@gmail.com>
2023-08-10 17:35:39 +0200

    logica sui punteggi (reset-points sul 5)

--- progetto.js
+++ progetto.js
@@ -2,12 +2,16 @@ let mossa = ["carta", "sasso", "forbice"];
 
 let playerSelection = "";
 let computerSelection = "";
+let playerPoints = 0;
+let computerPoints = 0;
+let isFinished = false;
 //let string_result;
 computerSelection = getComputerChoice();
 
 const buttonRock = document.getElementById("btn-rock");
 const buttonPaper = document.getElementById("btn-paper");
 const buttonScissor = document.getElementById("btn-scissor");
+const buttonReset = document.getElementById("btn-reset");
 
 //"accediamo" al paragarafo p che verrà modificato (dove sovrascriveremo il risultato con la stringa ottenuta)
 let output = document.getElementById("result-string");
@@ -15,45 +19,26 @@ let output = document.getElementById("result-string");
 let mossaGiocatore = document.getElementById("mossa-giocatore");
 let mossaComputer = document.getElementById("mossa-computer");
 //output.innerHTML = playRound(playerSelection, computerSelection);
-/*
-const risultato = document.createElement("p");
-const node = document.createTextNode(string_result);
-risultato.appendChild(node);
-const element = document.getElementById("string-div");
-element.appendChild(risultato);
-*/
-
 
 buttonRock.addEventListener("click", ()=>{
     playerSelection = buttonRock.textContent;
     //console.log(playRound(playerSelection, computerSelection));
-    output.innerHTML = playRound(playerSelection, computerSelection);
-    mossaGiocatore.innerHTML = playerSelection;
-    mossaComputer.innerHTML = computerSelection;
+    getButton();
 })
 
 buttonPaper.addEventListener("click", ()=>{
     playerSelection = buttonPaper.textContent;
     //console.log(playRound(playerSelection, computerSelection));
-    output.innerHTML = playRound(playerSelection, computerSelection);
-    mossaGiocatore.innerHTML = playerSelection;
-    mossaComputer.innerHTML = computerSelection;
+    getButton();
 })
 
 buttonScissor.addEventListener("click", ()=>{
     playerSelection = buttonScissor.textContent;
     //console.log(playRound(playerSelection, computerSelection));
-    output.innerHTML = playRound(playerSelection, computerSelection);
-    mossaGiocatore.innerHTML = playerSelection;
-    mossaComputer.innerHTML = computerSelection;
+    getButton();
 })
 
 //game();
-
-/*
-console.log(playerSelection, computerSelection)
-console.log(playRound(playerSelection, computerSelection));
-*/
 
 //funzione che ritorna la mossa (in modo casuale) del computer
 function getComputerChoice(){
@@ -72,27 +57,33 @@ function playRound(playerSelection, computerSelection){
         switch(computerSelection){
         case "sasso":
             string_result = "Hai vinto! Carta batte sasso";
+            playerPoints++
             break;
         case "forbice":
             string_result = "Hai Perso! Forbice batte carta";
+            computerPoints++
             break;
         }
     } else if (playerSelection == "sasso"){
         switch(computerSelection){
         case "forbice":
             string_result = "Hai vinto! Sasso batte forbice";
+            playerPoints++
             break;
         case "carta":
             string_result = "Hai Perso! Carta batte sasso";
+            computerPoints++
             break;
         }
     } else {
         switch(computerSelection){
         case "carta":
             string_result = "Hai vinto! Forbice batte carta";
+            playerPoints++
             break;
         case "sasso":
             string_result = "Hai Perso! Sasso batte forbice";
+            computerPoints++
             break;
         }
     }
@@ -113,3 +104,33 @@ function game(){
     }
 }
 */
+
+//verifichiamo se il punteggio del player o del computer è 5, se si: azzeriamo i punteggi (dobbiamo restituire un array per gestire il ritorno di piu valori da parte di una funzione)
+function isFive(player_points, computer_points){
+    if (player_points == 6 || computer_points == 6){
+        isFinished = true;
+        /*player_points = 0;
+        computer_points = 0;
+    }
+    return [player_points, computer_points];*/
+    }
+    return isFinished;
+}
+
+function setToZero(bool){
+    if (bool == true){
+        isFinished = false;
+        playerPoints = 0;
+        computerPoints = 0;
+    }
+    return [playerPoints, computerPoints];
+}
+
+function getButton(){
+    output.innerHTML = playRound(playerSelection, computerSelection);
+    mossaGiocatore.innerHTML = playerSelection;
+    mossaComputer.innerHTML = computerSelection;
+    isFinished = isFive(playerPoints, computerPoints)
+    points = setToZero(isFinished)
+    //console.log(points[0], points[1], isFinished);
+}
